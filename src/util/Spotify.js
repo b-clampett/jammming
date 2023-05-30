ebbe66537c6cd6b3fce9ada08cce3d4f876e7ebb
b-clampett <brian.t.clampett@gmail.com>
2023-05-30 13:20:38 -0400

    changed redirect uri to netlify url

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-const redirectUri = "http://localhost:3000";
+const redirectUri = "https://jammmmingproj.netlify.app/";
 const baseUrl = "https://accounts.spotify.com/authorize";
 
 const Spotify = {
